20658966307c1d70e0a582d706227e46c37bf615
シンエイ <157171073+Shineii86@users.noreply.github.com>
2025-07-12 15:06:50 +0530

    Create random.js

---
+++ api/v2/random.js
@@ -0,0 +1,13 @@
+const data = require("../../data/anime.json");
+
+export default function handler(req, res) {
+  const anime = data[Math.floor(Math.random() * data.length)];
+  res.json({
+    ...anime,
+    creator: "Shinei Nouzen",
+    github: "https://github.com/Shineii86",
+    telegram: "https://telegran.me/Shineii86",
+    message: "Build with ❤️ by Shinei Nouzen",
+    timestamp: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+  });
+}
