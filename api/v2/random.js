2156d8e1655b0bb792852f1e7e01750418ccc2f0
シンエイ <157171073+Shineii86@users.noreply.github.com>
2025-07-13 23:34:45 +0530

    Update random.js

--- api/v2/random.js
+++ api/v2/random.js
@@ -1,13 +1,22 @@
-const data = require("../../data/anime.json");
+import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "data", "anime.json");
+  const rawData = fs.readFileSync(filePath, "utf8");
+  const data = JSON.parse(rawData);
+
   const anime = data[Math.floor(Math.random() * data.length)];
+
   res.json({
     ...anime,
     creator: "Shinei Nouzen",
     github: "https://github.com/Shineii86",
     telegram: "https://telegran.me/Shineii86",
     message: "Build with ❤️ by Shinei Nouzen",
-    timestamp: new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+    timestamp: new Date().toLocaleString("en-IN", {
+      timeZone: "Asia/Kolkata",
+      hour12: true
+    })
   });
 }
