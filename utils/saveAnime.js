557aa7e7da7a38eeea3eeab479de846cc8618464
シンエイ <157171073+Shineii86@users.noreply.github.com>
2025-07-13 23:01:09 +0530

    Update saveAnime.js

--- utils/saveAnime.js
+++ utils/saveAnime.js
@@ -1,10 +1,10 @@
-import { readJson, writeJson } from "fs-extra";
+import fs from "fs-extra";
 import { join } from "path";
 
 const FILE_PATH = join(process.cwd(), "data", "anime.json");
 
 export async function saveUniqueAnime(animeList) {
-  const existing = await readJson(FILE_PATH).catch(() => []);
+  const existing = await fs.readJson(FILE_PATH).catch(() => []);
   const existingTitles = new Set(existing.map((a) => a.title));
 
   const newAnime = animeList.filter((a) => !existingTitles.has(a.title)).map((anime) => ({
@@ -20,7 +20,7 @@ export async function saveUniqueAnime(animeList) {
   }));
 
   const finalList = [...existing, ...newAnime];
-  await writeJson(FILE_PATH, finalList, { spaces: 2 });
+  await fs.writeJson(FILE_PATH, finalList, { spaces: 2 });
 
   return newAnime.length;
 }
