d69bd2f1bb8da3cc39187e8376844905bbeb2669
シンエイ <157171073+Shineii86@users.noreply.github.com>
2025-07-13 22:53:06 +0530

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const key = query?.key;
 
   if (key !== "Quinx") {
-    return res.status(401).json({ error: "Invalid API Key" });
+    return res.status(401).json({ error: "👀 Invalid ${API_KEY}" });
   }
 
   let allAnime = [];
